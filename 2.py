0d72a34a0158bf92c720f80a2d65c0117bcc051f
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-04-16 06:46:00 +0300

    new

--- 2.py
+++ 2.py
@@ -1,20 +1,27 @@
 import sys
 
 def fibonacci(n):
-    if n <= 1:
-        return n
+    if n <= 0:
+        raise ValueError("Число має бути додатнім")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
     else:
         a, b = 0, 1
-        for _ in range(n - 1):
+        for _ in range(n - 2):
             a, b = b, a + b
-        return b
+        return a + b
 
-try:
-    n = int(sys.stdin.readline())
-    result = fibonacci(n)
-    print(f"Число Фібоначчі для {n} дорівнює {result}")
-except ValueError:
-    sys.stderr.write("Будь ласка, введіть ціле число.\n")
-    sys.exit(1)
+
+if __name__ == "__main__":
+    try:
+        n = int(input())
+        result = fibonacci(n)
+        print(result)
+        sys.exit(0)
+    except ValueError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
 
 
